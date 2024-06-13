3dd6f2140b5e73884c02148b17a0903b72838b52
Yuri Luiz <yuri.luitzkt@gmail.com>
2024-06-13 11:30:00 -0300

    finished

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,19 +1,20 @@
-import { Game } from '../App'
 import Produto from '../components/Produto'
+import { useGetJogosQuery } from '../services/api'
 
 import * as S from './styles'
 
-type Props = {
-  jogos: Game[]
-  adicionarAoCarrinho: (jogo: Game) => void
-}
+const Produtos = () => {
+  const { data, isLoading } = useGetJogosQuery()
 
-const Produtos = ({ jogos, adicionarAoCarrinho }: Props) => {
+  if (isLoading) {
+    return <h2>Carregando...</h2>
+  }
+
   return (
     <>
       <S.Produtos>
-        {jogos.map((game) => (
-          <Produto key={game.id} game={game} aoComprar={adicionarAoCarrinho} />
+        {data?.map((game) => (
+          <Produto key={game.id} game={game} />
         ))}
       </S.Produtos>
     </>
